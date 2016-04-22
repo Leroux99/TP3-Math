7432fb7e8e0b1e71d52a1e00fd10918ca09fbe67
YD <200755008@edu.clg.qc.ca>
2016-04-22 13:46:46 -0400

    Push_TP4_First

---
+++ TP4_LoiNormal/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TP4_LoiNormal
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_Réinitialiser_Click(object sender, EventArgs e)
+        {
+            textBox_Min.Text = "";
+            textBox_Max.Text = "";
+            textBox_Moy.Text = "";
+            textBox_ET.Text = "";
+            textBox_Prob.Text = "";
+        }
+    }
+}
